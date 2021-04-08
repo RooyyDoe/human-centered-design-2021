4ad608879832db54a6855cc65b57a9a550fb4a8a
Rooyydoe <roykuijper199716@hotmail.com>
2021-04-08 11:27:56 +0200

    basic layout chat app

---
+++ public/js/chat.js
@@ -0,0 +1,85 @@
+const socket = io();
+
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message')
+// const name = document.getElementById('name')
+const btn = document.getElementById('send-button')
+const output = document.getElementById('message-output-list')
+const feedback = document.getElementById('new-feedback')
+
+let name = prompt('What is your name?')
+let timeout
+
+if (name == undefined || name == '') {
+    name = 'Guest';
+}
+
+appendMessage('You joined', 'join')
+
+socket.emit('new-user', name)
+
+
+socket.on('own-chat-message', message => {
+    feedback.innerText = ''
+    appendMessage(`${message}`, 'own-message')
+})
+
+socket.on('their-chat-message', data => {
+    feedback.innerText = ''
+    appendMessage(`${data.name}: ${data.message}`, 'their-message')
+})
+
+socket.on('user-connected', name => {
+    appendMessage(`Say hello to ${name}`, 'connect')
+})
+
+socket.on('user-disconnected', name => {
+    console.log('false?', name)
+    appendMessage(`${name} left the server`, 'disconnect')
+})
+
+socket.on('someone-is-typing', name => {
+    if (name) {
+        feedback.innerText = `${name} is typing a message...`
+        console.log('test')
+    } else {
+        feedback.innerText = ''
+    }
+})
+
+messageForm.addEventListener('submit', event => {
+    event.preventDefault()
+    const message = messageInput.value
+    socket.emit('send-message', message)
+    messageInput.value = ''
+})
+
+messageInput.addEventListener('keypress', () => {
+    socket.emit('typing-message', name)
+    console.log(name)
+    clearTimeout(timeout)
+    timeout = setTimeout(timeoutFunction, 1000)
+})
+
+function timeoutFunction() {
+    socket.emit("typing-message", false);
+}
+
+function appendMessage(message, style) {
+    const messageElement = document.createElement('div')
+    messageElement.classList.add('message-output')
+    messageElement.classList.add(style);
+
+    const newMessage = document.createElement('p')
+    newMessage.classList.add('new-message')
+    newMessage.innerText = message
+
+    output.append(messageElement)
+    messageElement.append(newMessage)
+    scrollDown()
+}
+
+function scrollDown() {
+    //https://exceptionshub.com/scroll-automatically-to-the-bottom-of-the-page.html
+    window.scrollTo(0, document.body.scrollHeight);
+}
---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const hbs = require("express-handlebars");
+const path = require('path');
+
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const port = process.env.PORT || 5000;
+
+
+app.use(express.static(path.resolve("public")));
+
+const users = {}
+
+io.on('connection', socket => {
+    console.log('Made socket working')
+
+    socket.on('new-user', name => {
+        users[socket.id] = name
+        socket.broadcast.emit('user-connected', name)
+    })
+
+    socket.on('disconnect', name => {
+        socket.broadcast.emit('user-disconnected', users[socket.id])
+        delete users[socket.id]
+    })
+
+    socket.on('send-message', message => {
+        socket.broadcast.emit('their-chat-message', { message: message, name: users[socket.id] })
+        socket.emit('own-chat-message', message);
+    })
+
+    socket.on('typing-message', name => {
+        socket.broadcast.emit('someone-is-typing', name)
+    })
+});
+
+// template engine
+app.set("view engine", "hbs")
+// Tell the views engine/ejs where the template files are stored (Settingname, value)
+app.engine('hbs', hbs({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    partialsDir: __dirname + '/views/partials/'
+}))
+
+app.get('/', function (req, res) {
+    res.render('home.hbs')
+})
+
+http.listen(port, () => console.log(`Example app listening on port ${port}!`));
